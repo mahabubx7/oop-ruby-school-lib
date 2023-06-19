00fcd6c31a404e62c275fe7d9cd1ee555a480e93
Mahabub Alam <mahabubx7@gmail.com>
2023-06-19 22:24:38 +0600

    Class(person): created-person-class-with-tests

---
+++ spec/person_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../lib/person'
+
+describe 'Unit tests for "Person" class' do
+  it 'should return name & age' do
+    person1 = Person.new(25, 'Mahabub')
+    person2 = Person.new(17, 'Mahabub2')
+
+    expect(person1.name).to eq('Mahabub')
+    expect(person1.age).to eq(25)
+    
+    expect(person2.name).to eq('Mahabub2')
+    expect(person2.age).to eq(17)
+  end
+
+  it 'should check person can_use_service?' do
+    person1 = Person.new(25, 'Mahabub')
+    person2 = Person.new(17, 'Mahabub2', false)
+    expect(person1.can_use_service?).to eq(true)
+    expect(person2.can_use_service?).to eq(false)
+  end
+end
