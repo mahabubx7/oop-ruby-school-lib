f55ef9b429710bf43ca3951fbc03ccf96b6d97ce
Mahabub Alam <mahabubx7@gmail.com>
2023-06-21 02:45:01 +0600

    Class(book): added-book-with-tests

---
+++ spec/book_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/book.rb'
+
+describe 'Unit tests for "Book" class' do
+  book = Book.new('Ruby and OOP', 'F. Microverse')
+
+  it 'should return a book title' do
+    expect(book.title).to eq('Ruby and OOP')
+  end
+
+  it 'should return a book author' do
+    expect(book.author).to eq('F. Microverse')
+  end
+end
